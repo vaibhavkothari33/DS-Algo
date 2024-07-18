bcb379eb57cbc0b2cf0edf68652504347a3324c1
vaibhavkothari33 <vaibhavkothari50@gmail.com>
2024-07-19 00:02:55 +0530

    arrays

---
+++ Array/power.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int mySqrt(int x) {
+        if(x==0){
+            return 1;
+        }
+        else if(x==1){
+            return x;
+        }
+        else{
+            
+        }
+    }
+};
+int main()
+{
+     int x;
+     cin>>x;
+
+}
