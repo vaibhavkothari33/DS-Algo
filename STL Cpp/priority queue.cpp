b2eb805d9489d5660dc83192d8a30e8f8724e8d2
Vaibhav Kothari <129139486+vaibhavkothari33@users.noreply.github.com>
2024-07-29 03:34:01 +0000

    priority queue

---
+++ STL Cpp/priority queue.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    // max heap
+    priority_queue<int> max ;
+
+    priority_queue<int,vector<int>,greater<int>min;
+    maxi.push(1);
+    maxi.push(2);
+    maxi.push(4);
+    maxi.push(0);
+    int n=max.size();
+    for(int i=0;i<=n;i++){
+        cout<<maxi.top()<<" "; //4 2 1 0
+        maxi.pop();
+    }
+    // cout<<i<<" ";
+
+    for(int i :l)
+}
