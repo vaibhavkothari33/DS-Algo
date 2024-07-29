26062e560c5e4cb9e8331e4ef8c4f1eeaa0c4e81
Vaibhav Kothari <129139486+vaibhavkothari33@users.noreply.github.com>
2024-07-29 18:56:47 +0000

    STL

--- STL Cpp/map.cpp
+++ STL Cpp/map.cpp
@@ -8,4 +8,21 @@ int main(){
 
     m[1]="Love";
     m[2]="Babbar";
+    m[13]="Vaibhav";
+    m.insert(5,"Bheem");
+
+
+    for(auto i:m){
+        cout<<i.first<<" "i.second<<endl;
+    }
+
+    m.erase(13);
+    cout<<"finding 132 --> "m.count(13)<<endl;
+    // Time---> O(log n)
+
+    
+    auto it = m.find(5);
+    for(auto i=it;i!=m.end();i++){
+        cout<<(*i).first<<endl;
+    }
 }
