d0990028840dbbf1c7b44306801b54b260ca1811
vaibhavkothari33 <vaibhavkothari50@gmail.com>
2024-09-22 16:38:01 +0530

    pointers

--- pointers/one.cpp
+++ pointers/one.cpp
@@ -5,19 +5,26 @@ int main()
 {
     int n = 5;
     int *ptr = &n;
-    cout << n << endl;
-    // address of operator - > &
-    cout << &n;
+    int vai = 9;
+    int *ptr1 = &vai;
+    // cout << n << endl;
+    // // address of operator - > &
+    // cout << &n<< endl;
+    // cout << ptr<< endl;
+    // cout << *ptr<< endl;
+    // cout << &ptr<< endl;
+    // cout<<"New pointer"<<endl;
+    // cout << &ptr1<< endl;
+    // cout << ++*ptr1<< endl;
 
-    cout << endl;
+    // copying of the pointer
+    int num = 7;
+    int *ptr2 = &num;
+    int *q = ptr2;
+    cout<<*ptr2<<endl<<q<<endl;
+    cout<<&ptr2<<endl<<*q<<endl;
+    cout<<&num<<endl;
+    // cout<<**num<<endl;
 
-    cout << ptr;
-
-    cout << endl;
-
-    cout << *ptr;
-
-    cout << endl;
-
-    cout << &ptr;
+ 
 }
---
+++ pointers/pointer_maths.cpp
@@ -0,0 +1,40 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main()
+// {
+//     //  int i=3;
+//     //  int *t = &i;
+//     //  cout<<(*t)++<<endl;
+//     //  cout<<(*t)++<<endl;
+//     //  cout<<++(*t)<<endl;
+//     int arr[10];
+//     char ch[6] = "abcde";
+//     // cout << ch << endl;
+//     char *c = &ch[0];
+//     cout << c << endl;
+//     cout << "Address of first memory: " << arr << endl;
+//     cout << "Address of first memory: " << &arr << endl;
+//     cout << "Value of first memory: " << *arr << endl;
+// }
+
+#include <bits/stdc++.h>
+using namespace std;
+void print(int *p)
+{
+    cout << *p << endl; 
+}
+void update(int *p){
+    *p = *p+1;
+}
+int main()
+{
+    int value = 5;
+    int *p = &value;
+
+    // print(p);
+    cout<<*p<<endl;
+    update(p);
+    print(p);
+    cout<<*p<<endl;
+}
