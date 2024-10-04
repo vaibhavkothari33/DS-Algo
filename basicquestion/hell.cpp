487488ea15b136eca2025500e2402b6aa02f65f3
vaibhavkothari33 <vaibhavkothari50@gmail.com>
2024-10-04 16:14:08 +0530

    more

--- basicquestion/hell.cpp
+++ basicquestion/hell.cpp
@@ -1,10 +1,10 @@
 // #include <iostream>
 // using namespace std;
 // int main() {
-//     cout << "Size of int: " << sizeof(int) << " bytes" << std::endl;
-//     cout << "Size of char: " << sizeof(char) << " bytes" << std::endl;
-//     cout << "Size of float: " << sizeof(float) << " bytes" << std::endl;
-//     cout << "Size of double: " << sizeof(double) << " bytes" << std::endl;
+//     cout << "Size of int: " << sizeof(int) << " bytes" << endl;
+//     cout << "Size of char: " << sizeof(char) << " bytes" << endl;
+//     cout << "Size of float: " << sizeof(float) << " bytes" << endl;
+//     cout << "Size of double: " << sizeof(double) << " bytes" << endl;
 
 //     return 0;
 // }
@@ -32,20 +32,138 @@
 //     return 0;
 // }
 
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main()
+// {
+//     string sentence;
+//     cout << "Enter the sentence " << endl;
+//     getline(cin, sentence);
+//     stringstream vai(sentence);
+//     string word;
+
+//     while (vai >> word)
+//     {
+//         cout << word << endl;
+//     }
+//     return 0;
+// }
+
+// sum of n terms
+
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// int main()
+// {
+//     int n;
+//     cin>>n;
+//      int arr[n];
+//      int sum =0;
+//      for(int i=0;i<n;i++){
+//         cin>>arr[i];
+//         sum+=arr[i];
+//      }
+//      cout<<sum;
+// }
+
+// decimal to binary
+
+// #include <iostream>
+// using namespace std;
+// int main() {
+//     int decimalNum;
+//     cout << "Enter a decimal number: ";
+//     cin >> decimalNum;
+
+//     int binaryNum[32]; // Array to store the binary representation, assuming 32-bit integer
+//     int i = 0;
+
+//     while (decimalNum > 0) {
+//         binaryNum[i] = decimalNum % 2;
+//         decimalNum = decimalNum / 2;
+//         i++;
+//     }
+
+//     cout << "Binary representation: ";
+//     for (int j = i - 1; j >= 0; j--) {
+//         cout << binaryNum[j];
+//     }
+
+//     return 0;
+// }
+
+// reverse a number
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main()
+// {
+//     int n;
+//     cin >> n;
+//     int sum = 0;
+//     while (n > 0)
+//     {
+//         int last = n % 10;
+//         n = n / 10;
+//         sum = sum * 10 + last;
+//     }
+//     cout << sum;
+// }
+
+// GCD
+
+// #include<bits/stdc++.h>
+// using namespace std;
+// int gcd(int a,int b){
+//     if(b==0){
+//         return a;
+//     }
+//     else{
+//          return gcd(b,a%b);
+//     }
+// }
+// int main()
+// {
+//      int a,b;
+//      cin>>a;
+//      cin>>b;
+//      cout<<"the GCD of a and b is: "<<gcd(a,b);
+// }
+
+// Inheritance (Reusability)
+// Inheritance is a process of obtaining the data members and methods
+// from one class to another class, plus can have its own is known as
+// inheritance.
+
+// Polymorphism
+// The same entity (function or object) behaves differently in different
+// scenarios.
+
+// Abstraction
+// Hiding lower-level details and exposing only the essential and
+// relevant details to the users
+
+// Encapsulation
+// Wrapping data and methods in a single unit to prevents
+// un-authorized access from outside class.
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int main()
 {
-    string sentence;
-    cout << "Enter the sentence " << endl;
-    getline(cin, sentence);
-    stringstream vai(sentence);
-    string word;
-
-    while (vai >> word)
-    {
-        cout << word << endl;
-    }
-    return 0;
+    int a = 10;
+    int b = 20;
+    cout << "Before swap" << endl;
+    cout << a << " " << b << endl;
+    // int temp;
+    // temp = a;
+    // a=b;
+    // b = temp;
+    swap(a,b);
+    cout << "After swap" << endl;
+    cout << a << " " << b << endl;
 }
