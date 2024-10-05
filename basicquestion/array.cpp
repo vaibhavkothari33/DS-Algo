618bf37fcc215fcb9bef6bbcec873a79bf1a1082
vaibhavkothari33 <vaibhavkothari50@gmail.com>
2024-10-05 14:35:33 +0530

    code forces ka question1

--- basicquestion/array.cpp
+++ basicquestion/array.cpp
@@ -1,31 +1,64 @@
-#include <bits/stdc++.h>
-using namespace std;
+// #include <bits/stdc++.h>
+// using namespace std;
 
+// int main()
+// {
+//     int arr[] = {1, 2, 4, 5, 6};
+//     int n = sizeof(arr) / sizeof(arr[0]);
+//     cout << sizeof(arr) << endl;
+//     cout << sizeof(arr[0]) << endl;
+//     cout << n << endl;
+
+//     for (int i = 0; i < n; i++)
+//     {
+//         cout << arr[i] << " ";
+//     }
+//     cout<<endl;
+
+//     int newele = 52;
+
+//     for(int i=n;i<1;i++){
+//         arr[i-1] = arr[i];
+//     }
+
+//     arr[0] = newele;
+
+//     for (int i = 0; i < n; i++)
+//     {
+//         cout << arr[i] << " ";
+//     }
+
+// }
+
+#include <iostream>
+using namespace std;
 int main()
 {
-    int arr[] = {1, 2, 4, 5, 6};
-    int n = sizeof(arr) / sizeof(arr[0]);
-    cout << sizeof(arr) << endl;
-    cout << sizeof(arr[0]) << endl;
-    cout << n << endl;
-
-    for (int i = 0; i < n; i++)
+    int arr[4];
+    for (int i = 0; i < 4; i++)
     {
-        cout << arr[i] << " ";
+        cin >> arr[i];
     }
-    cout<<endl;
-    // n=n+1;
-    int newele = 52;
-
-    for(int i=n;i<1;i++){
-        arr[i-1] = arr[i];
+    // l1 r1 l2 r2
+    if(arr[1]<arr[2]){
+        cout<<-1;
     }
-
-    arr[0] = newele;
-
-    for (int i = 0; i < n; i++)
-    {
-        cout << arr[i] << " ";
+    else if(arr[3]<arr[0]){
+        cout<<-1;
     }
-
+    else if(arr[0]<=arr[2] && arr[1]<=arr[3]){
+        cout<<arr[2]<<" "<<arr[1]<<endl;
+    }
+    else if(arr[2]<=arr[0] && arr[3]<=arr[1]){
+        cout<<arr[0]<<" "<<arr[3]<<endl;
+    }
+    else if(arr[0]<=arr[2] && arr[3]<=arr[1]){
+        cout<<arr[2]<<" "<<arr[3]<<endl;
+    }
+    else if(arr[2]<=arr[0] && arr[1]<=arr[3]){
+        cout<<arr[0]<<" "<<arr[1]<<endl;
+    }
+    else{
+        cout<<-1;
+    }
 }
---
+++ basicquestion/twodarray.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    int arr[n][m];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+    cout << endl;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    cout<<endl;
+    cout<<arr[1][0]<<endl;
+    cout<<arr[0][2]<<endl;
+    cout<<arr[1][2]<<endl;
+}
